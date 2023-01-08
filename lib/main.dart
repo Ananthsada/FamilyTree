e695c22536fa9dcab42ffaf269f7d78343fa07e2
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2023-01-09 02:37:07 +0800

    Trying stateless widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,27 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: Home()
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Family Tree',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        ),
-        home: MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                
-              },
-              child: Text('Generate Family Tree'),
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text('Family Tree'),
+        centerTitle: true,
       ),
-    );
-  }
-}
-
-class FamilyMember extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Text("New Member"),
+      body: Padding(
+        padding: EdgeInsets.all(30.0),
+        child: Text('hello'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Text("Button"),
+      ),
     );
   }
 }

ccd4cda6bf9e7e29762d6372b2059ce4a1f77959
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2023-01-22 01:53:50 +0800

    Visualize graphs

--- README.md
+++
@@ -1,7 +0,0 @@
-# familytree
-
-Tool to visualize family tree.
-
-## How to run
-
-- As of now, I intend to build this only as web app.
--- lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'Graph.dart';
-
-void main() {
-  runApp(MaterialApp(
-    home: GraphView()
-  ));
-}
